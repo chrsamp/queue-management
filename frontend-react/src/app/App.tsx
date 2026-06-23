cbec0ac2d7f88a97a4fe1bc9278e48ad4d32ae0c
Chris Sampson <chris.sampson@gov.bc.ca>
2026-06-23 09:51:02 -0700

    Add app shell components

--- frontend-react/src/app/App.tsx
+++ frontend-react/src/app/App.tsx
@@ -1,8 +1,19 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+
 function App() {
   return (
-    <>
-      <h1>Service BC Queue Management</h1>
-    </>
+    <div className="flex min-h-screen flex-col">
+      <Header
+        skipLinks={[{ href: '#main', label: 'Skip to main content' }]}
+        title="Service BC Queue Management"
+        titleAs="h1"
+      />
+      <main className="flex-1 p-4" id="main">
+        <h2>App</h2>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
