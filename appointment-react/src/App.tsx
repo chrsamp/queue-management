6aaf5254569b06e386f98548bb88df9940a284f7
Chris Sampson <chris.sampson@gov.bc.ca>
2026-06-30 08:53:50 -0700

    Add appointment scaffold

---
+++ appointment-react/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center gap-4">
+        <h1>Book a Service BC Appointment</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ appointment-react/vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="vitest/config" />
+
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 8100,
+    strictPort: true,
+  },
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  test: {
+    environment: 'jsdom',
+    include: ['src/**/*.test.{ts,tsx}'],
+    setupFiles: './src/test/setup.ts',
+  },
+})
